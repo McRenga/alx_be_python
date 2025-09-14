2dcc4b570271e364cb3ab8ecd06bb6c237add6f0
McRenga <rengalaino@gmail.com>
2025-09-14 23:35:33 +0300

    Add basic operations script for addition, subtraction, and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+
+print(f"Adition of {number1} and {number2} is {number1 + number2} ")
+print(f"subtraction of {number2} and {number1} is {number2-number1}")
+print(f"multiplication of {number1} and {number2} is {number1*number2}")
